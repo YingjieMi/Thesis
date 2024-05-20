e211cd7b566a2c83419d1f091286471a56b33c2e
YingjieMi <yimi8952@uni.sydney.edu.au>
2024-05-20 21:13:40 +1000

    Modify comment

--- SLAM/EKF_SLAM-master/EKF_SLAM_simulation.m
+++ SLAM/EKF_SLAM-master/EKF_SLAM_simulation.m
@@ -1,56 +1,56 @@
-% EKF_SLAM x,P 丢失 仿真文件
+% EKF_SLAM x, P Loss Simulation File
 close all
 clear
 clc;
 
-% 加载模型参数
+% Load simulation configuration parameters
 simulation_config
 
-% 添加文件夹
+% Add folders to the path
 addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\data');
 addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\functions');
-addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\pictrues');
+addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\pictures');
 
-% 加载数据
+% Load map data
 MAP_DATA = 'data/map3.mat';
 load(MAP_DATA)
 
-% 设置可变参数：丢包率和协方差scale
-intermit = 0;                   % 是否使用中断算法  0不用；1用
+% Set variable parameters: packet loss rate and covariance scale
+intermit = 0;                   % Whether to use intermittent algorithm 0: No; 1: Yes
 packet_loss_prob = 0.0;
-scaleFactor = 1.05;        %1.05
-    
-% 加载关键点
+scaleFactor = 1.05;             % Scale factor for covariance
+
+% Load key points
 key_points = data_original.key_points;
-% 加载路标
+% Load landmarks
 landmarks = data_original.landmarks;
-% 加载状态
+% Load states
 states = data_original.states;
-% 加载路径关键点
+% Load waypoints
 wp = data_original.key_points;
 
-% 获取序列长度
-length = size(states,2);
+% Get the length of the sequence
+length = size(states, 2);
 
 if ADD_COLOR_NOISE == 1
-    noise_V = gen_color_noise(length,Q(1,1),c);
-    noise_G = gen_color_noise(length,Q(2,2),c);
-    for i = 1:1:length
+    noise_V = gen_color_noise(length, Q(1,1), c);
+    noise_G = gen_color_noise(length, Q(2,2), c);
+    for i = 1:length
         states(i).Vn = states(i).V + noise_V(i);
         states(i).Gn = states(i).G + noise_G(i);
     end
 end
 
-true_trajectory = zeros(3,length);
-model_pre_trajectory = zeros(3,length);
-EKF_pre_trajectory = zeros(3,length);
+true_trajectory = zeros(3, length);
+model_pre_trajectory = zeros(3, length);
+EKF_pre_trajectory = zeros(3, length);
 
-x= states(1).xtrue; % 状态向量
-P= zeros(3); % 协方差矩阵
-QE= 2*Q; % 控制变量的协方差矩阵
-RE= 2*R; % 传感器协方差矩阵
-ftag= 1:size(landmarks,2);
-da_table= zeros(1,size(landmarks,2));
+x = states(1).xtrue; % State vector
+P = zeros(3); % Covariance matrix
+QE = 2 * Q; % Covariance matrix for control variables
+RE = 2 * R; % Sensor covariance matrix
+ftag = 1:size(landmarks, 2);
+da_table = zeros(1, size(landmarks, 2));
 dt = DT_CONTROLS;
 
 if ASYNCHRONOUS == 1
@@ -60,155 +60,151 @@ end
 x_model_pre = x;
 
 if SLAM_SAVE_GIF == 1
-
-    if exist('ekf_slam.avi','file') == 2
+    if exist('ekf_slam.avi', 'file') == 2
         delete('ekf_slam.avi');
     end
-    
-    if exist('ekf_slam.gif','file') == 2
+
+    if exist('ekf_slam.gif', 'file') == 2
         delete('ekf_slam.gif');
     end
-    
-    %创建avi文件对象
-    aviobj = VideoWriter('ekf_slam.avi','Uncompressed AVI');
+
+    % Create AVI file object
+    aviobj = VideoWriter('ekf_slam.avi', 'Uncompressed AVI');
     open(aviobj)
 end
 
 %%
-% 循环算法仿真
+% Loop for simulation
 fig = figure;
 hold on;
 
 last_x = x;
 last_P = P;
 
-% 设置种子为1以获得可复现的随机数序列
-rng(1); %3
+% Set the seed to 1 for reproducible random number sequence
+rng(1); % 3
 
-for k = 1:1:length
-    % 获取控制量
+for k = 1:length
+    % Get control inputs
     Vn = states(k).Vn;
     Gn = states(k).Gn;   
-    % 存储控制量
+    % Store control inputs
     sim_result.controls(k).Vn = Vn;
     sim_result.controls(k).Gn = Gn;
     
-    % 使用上一次的状态和协方差
+    % Use the last state and covariance
     x = last_x;
     P = last_P;
 
-    % 随机决定是否丢包
+    % Randomly decide whether to lose packet
     if rand < packet_loss_prob && k ~= 1
         packetLost = 1;
     else
         packetLost = 0;
     end
     
-    % 预测阶段
-    [x,P] = EKF_predict (x,P, Vn,Gn,QE, WHEELBASE,dt);
-    % 获取仅通过模型预测的位姿
-    x_model_pre = vehicle_model(x_model_pre, Vn,Gn, WHEELBASE,dt);
-    
+    % Prediction phase
+    [x, P] = EKF_predict(x, P, Vn, Gn, QE, WHEELBASE, dt);
+    % Get the pose predicted only by the model
+    x_model_pre = vehicle_model(x_model_pre, Vn, Gn, WHEELBASE, dt);
 
     if states(k).observation_update == 1
         if packetLost == 0
-            % 获取观测值
+            % Get observations
             z = states(k).zn;
             ftag_visible = states(k).ftag_visible;
         else
             P = P * scaleFactor;
         end
         
-        % 数据关联
+        % Data association
         if SWITCH_ASSOCIATION_KNOWN == 1
-            [zf,idf,zn, da_table]= data_associate_known(x,z,ftag_visible, da_table);
+            [zf, idf, zn, da_table] = data_associate_known(x, z, ftag_visible, da_table);
         else
-            [zf,idf, zn]= data_associate(x,P,z,RE, GATE_REJECT, GATE_AUGMENT); 
+            [zf, idf, zn] = data_associate(x, P, z, RE, GATE_REJECT, GATE_AUGMENT); 
         end
         
-        % ---------------------------------------------------------
-        % 更新状态向量
+        % Update state vector
         if SWITCH_USE_IEKF == 1 
             if intermit == 1
-                [x,P]= IEKF_update_Intermittent(x,P,zf,RE,idf, 3, ~packetLost);
+                [x, P] = IEKF_update_Intermittent(x, P, zf, RE, idf, 3, ~packetLost);
             else
-                [x,P]= update_iekf(x,P,zf,RE,idf, 3);
+                [x, P] = update_iekf(x, P, zf, RE, idf, 3);
             end
         else
             if intermit == 1
-                [x,P]= EKF_update_Intermittent(x,P,zf,RE,idf, 1, ~packetLost);
+                [x, P] = EKF_update_Intermittent(x, P, zf, RE, idf, 1, ~packetLost);
             else
-                [x,P]= EKF_update(x,P,zf,RE,idf, 1); 
+                [x, P] = EKF_update(x, P, zf, RE, idf, 1); 
             end
         end
-        % ---------------------------------------------------------
 
-        % 添加新的landmark到状态向量中
-        [x,P]= augment(x,P, zn,RE); 
+        % Add new landmark to state vector
+        [x, P] = augment(x, P, zn, RE); 
     end
 
-    % 保存当前状态和协方差，以便下一次循环使用
+    % Save the current state and covariance for the next iteration
     last_x = x;
     last_P = P;
     
     xtrue = states(k).xtrue;
     iwp = states(k).next_keypoint;
     
-    % 清除图像
+    % Clear the plot
     cla;
     axis equal
    
-    true_trajectory(:,k) = xtrue(1:3);
-    model_pre_trajectory(:,k) = x_model_pre(1:3);
-    EKF_pre_trajectory(:,k) = x(1:3);
+    true_trajectory(:, k) = xtrue(1:3);
+    model_pre_trajectory(:, k) = x_model_pre(1:3);
+    EKF_pre_trajectory(:, k) = x(1:3);
     
-    % 画出历史轨迹
-    plot( true_trajectory(1, 1:k), true_trajectory(2, 1:k), 'k--','linewidth',3);
+    % Plot the historical trajectory
+    plot(true_trajectory(1, 1:k), true_trajectory(2, 1:k), 'k--', 'linewidth', 3);
     
-    % 画出历史EKF预测轨迹
-    plot( EKF_pre_trajectory(1, 1:k), EKF_pre_trajectory(2, 1:k), 'r','linewidth',3 );
+    % Plot the historical EKF predicted trajectory
+    plot(EKF_pre_trajectory(1, 1:k), EKF_pre_trajectory(2, 1:k), 'r', 'linewidth', 3);
 
-    % 画出历史model预测轨迹
-    plot( model_pre_trajectory(1, 1:k), model_pre_trajectory(2, 1:k), 'b-.','linewidth',3);
+    % Plot the historical model predicted trajectory
+    plot(model_pre_trajectory(1, 1:k), model_pre_trajectory(2, 1:k), 'b-.', 'linewidth', 3);
     
-     % 画出landmarks
-    scatter( landmarks(1, :), landmarks(2, :), 'b*' );
+    % Plot the landmarks
+    scatter(landmarks(1, :), landmarks(2, :), 'b*');
     
-    % 画出路径关键点
-    plot( wp(1,:), wp(2, :), 'r.','markersize',26 );
+    % Plot the waypoints
+    plot(wp(1,:), wp(2,:), 'r.', 'markersize', 26);
     
-    % 画出目标点的位置
-    if iwp~=0
-       plot(wp(1,iwp),wp(2,iwp),'bo','markersize',13,'linewidth',1);
+    % Plot the position of the target point
+    if iwp ~= 0
+       plot(wp(1, iwp), wp(2, iwp), 'bo', 'markersize', 13, 'linewidth', 1);
     end
     
-    % 画出车的位姿
-    draw_car(xtrue,5,'k');
+    % Plot the vehicle pose
+    draw_car(xtrue, 5, 'k');
     
-    % EKF预测位姿
-    draw_car(x,5,'r');
+    % EKF predicted pose
+    draw_car(x, 5, 'r');
 
-    % 模型预测位姿
-    draw_car(x_model_pre,5,'g');
+    % Model predicted pose
+    draw_car(x_model_pre, 5, 'g');
     
-    legend('true','ekf','model');
+    legend('true', 'ekf', 'model');
 
     pause(0.00000001)
     
     if SLAM_SAVE_GIF == 1
-        %获取当前画面
+        % Get the current frame
         F = getframe(fig);
-        %加入avi对象中
-        writeVideo(aviobj,F);
+        % Add to AVI object
+        writeVideo(aviobj, F);
         
-        %转成gif图片,只能用256色
+        % Convert to GIF image, only 256 colors can be used
         im = frame2im(F);
-        [I,map] = rgb2ind(im,256);
-        %写入 GIF89a 格式文件   
+        [I, map] = rgb2ind(im, 256);
+        % Write to GIF89a format file   
         if k == 1
-            imwrite(I,map,'ekf_slam.gif','GIF', 'Loopcount',inf,'DelayTime',0.1);
+            imwrite(I, map, 'ekf_slam.gif', 'GIF', 'Loopcount', inf, 'DelayTime', 0.1);
         else
-            imwrite(I,map,'ekf_slam.gif','GIF','WriteMode','append','DelayTime',0.1);
+            imwrite(I, map, 'ekf_slam.gif', 'GIF', 'WriteMode', 'append', 'DelayTime', 0.1);
         end
     end 
     
@@ -219,17 +215,16 @@ for k = 1:1:length
     
 end
 
-
 %% Save results and data
 sim_result.landmarks = landmarks;
-sim_result.ture_trajectory = true_trajectory;
+sim_result.true_trajectory = true_trajectory;
 sim_result.EKF_pre_trajectory = EKF_pre_trajectory;
 sim_result.model_pre_trajectory = model_pre_trajectory;
 sim_result.wp = wp;
 save sim_result_original sim_result;
 
 % sim_result_int.landmarks = landmarks;
-% sim_result_int.ture_trajectory = true_trajectory;
+% sim_result_int.true_trajectory = true_trajectory;
 % sim_result_int.EKF_pre_trajectory = EKF_pre_trajectory;
 % sim_result_int.model_pre_trajectory = model_pre_trajectory;
 % sim_result_int.wp = wp;
--- SLAM/EKF_SLAM-master/EKF_lossx_simulation.m
+++ SLAM/EKF_SLAM-master/EKF_lossx_simulation.m
@@ -1,104 +1,103 @@
-% EKF_SLAM x,P 丢失 仿真文件
+% EKF_SLAM x, P Loss Simulation File
 close all
 clear
 clc;
 
-% 加载模型参数
+% Load simulation configuration parameters
 simulation_config
 
-% 添加文件夹
+% Add folders to the path
 addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\data');
 addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\functions');
-addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\pictrues');
+addpath('C:\Users\Microsoft微软\Desktop\Thesis\SLAM\EKF_SLAM-master\pictures');
 
-% 加载数据
+% Load map data
 MAP_DATA = 'data/map3.mat';
 load(MAP_DATA)
 
-% 设置可变参数：丢包率和协方差scale
+% Set variable parameters: packet loss rate and covariance scale
 packet_loss_prob = 0.3;
-scaleFactor = 1.05;        %1.05
-    
-% 加载关键点
+scaleFactor = 1.05;             % Scale factor for covariance
+
+% Load key points
 key_points = data_original.key_points;
-% 加载路标
+% Load landmarks
 landmarks = data_original.landmarks;
-% 加载状态
+% Load states
 states = data_original.states;
-% 加载路径关键点
+% Load waypoints
 wp = data_original.key_points;
 
-% 获取序列长度
-length = size(states,2);
+% Get the length of the sequence
+length = size(states, 2);
 
-true_trajectory = zeros(3,length);
-model_pre_trajectory = zeros(3,length);
-EKF_pre_trajectory = zeros(3,length);
-EKF_pre_trajectory_int = zeros(3,length);
+true_trajectory = zeros(3, length);
+model_pre_trajectory = zeros(3, length);
+EKF_pre_trajectory = zeros(3, length);
+EKF_pre_trajectory_int = zeros(3, length);
 
-% 不使用中断初始化
-x= states(1).xtrue; % 状态向量
-P= zeros(3); % 协方差矩阵
-QE= 2*Q; % 控制变量的协方差矩阵
-RE= 2*R; % 传感器协方差矩阵
-ftag= 1:size(landmarks,2);
-da_table= zeros(1,size(landmarks,2));
+% Initialize without intermittent
+x = states(1).xtrue; % State vector
+P = zeros(3); % Covariance matrix
+QE = 2 * Q; % Covariance matrix for control variables
+RE = 2 * R; % Sensor covariance matrix
+ftag = 1:size(landmarks, 2);
+da_table = zeros(1, size(landmarks, 2));
 dt = DT_CONTROLS;
 
-% 使用中断初始化
-x_int= states(1).xtrue; % 状态向量
-P_int= zeros(3); % 协方差矩阵
-da_table_int= zeros(1,size(landmarks,2));
+% Initialize with intermittent
+x_int = states(1).xtrue; % State vector
+P_int = zeros(3); % Covariance matrix
+da_table_int = zeros(1, size(landmarks, 2));
 
 x_model_pre = x;
 
 if SLAM_SAVE_GIF == 1
-
-    if exist('ekf_slam.avi','file') == 2
+    if exist('ekf_slam.avi', 'file') == 2
         delete('ekf_slam.avi');
     end
-    
-    if exist('ekf_slam.gif','file') == 2
+
+    if exist('ekf_slam.gif', 'file') == 2
         delete('ekf_slam.gif');
     end
-    
-    %创建avi文件对象
-    aviobj = VideoWriter('ekf_slam.avi','Uncompressed AVI');
+
+    % Create AVI file object
+    aviobj = VideoWriter('ekf_slam.avi', 'Uncompressed AVI');
     open(aviobj)
 end
 
 %%
-% 循环算法仿真
+% Loop for simulation
 fig = figure;
 hold on;
 
-% 设置种子为1以获得可复现的随机数序列
-rng(1); %1
+% Set the seed to 1 for reproducible random number sequence
+rng(1); % 1
 
-for k = 1:1:length
-    % 获取控制量
+for k = 1:length
+    % Get control inputs
     Vn = states(k).Vn;
     Gn = states(k).Gn;   
-    % 存储控制量
+    % Store control inputs
     sim_result.controls(k).Vn = Vn;
     sim_result.controls(k).Gn = Gn;   
 
-    % 随机决定是否丢包
+    % Randomly decide whether to lose packet
     if rand < packet_loss_prob && k ~= 1
         packetLost = 1;
     else
         packetLost = 0;
     end
     
-    % 预测阶段
-    % 不使用中断
-    [x,P] = EKF_predict (x,P, Vn,Gn,QE, WHEELBASE,dt);
-    % 使用中断
-    [x_int,P_int] = EKF_predict (x_int,P_int, Vn,Gn,QE, WHEELBASE,dt);
+    % Prediction phase
+    % Without intermittent
+    [x, P] = EKF_predict(x, P, Vn, Gn, QE, WHEELBASE, dt);
+    % With intermittent
+    [x_int, P_int] = EKF_predict(x_int, P_int, Vn, Gn, QE, WHEELBASE, dt);
 
     if states(k).observation_update == 1
         if packetLost == 0
-            % 获取观测值
+            % Get observations
             z = states(k).zn;
             ftag_visible = states(k).ftag_visible;
         else
@@ -106,84 +105,84 @@ for k = 1:1:length
             P_int = P_int * scaleFactor;
         end
         
-        % 数据关联
-        [zf,idf, zn]= data_associate(x,P,z,RE, GATE_REJECT, GATE_AUGMENT); 
-        [zf_int,idf_int, zn_int]= data_associate(x_int,P_int,z,RE, GATE_REJECT, GATE_AUGMENT); 
+        % Data association
+        [zf, idf, zn] = data_associate(x, P, z, RE, GATE_REJECT, GATE_AUGMENT); 
+        [zf_int, idf_int, zn_int] = data_associate(x_int, P_int, z, RE, GATE_REJECT, GATE_AUGMENT); 
 
         % ---------------------------------------------------------
-        % 更新状态向量
+        % Update state vector
         if SWITCH_USE_IEKF == 1 
-            [x_int,P_int]= IEKF_update_Intermittent(x_int,P_int,zf_int,RE,idf_int, 3, ~packetLost);
-            [x,P]= update_iekf(x,P,zf,RE,idf, 3);
+            [x_int, P_int] = IEKF_update_Intermittent(x_int, P_int, zf_int, RE, idf_int, 3, ~packetLost);
+            [x, P] = update_iekf(x, P, zf, RE, idf, 3);
         else
-            [x_int,P_int]= EKF_update_Intermittent(x_int,P_int,zf_int,RE,idf_int, 1, ~packetLost);
-            [x,P]= EKF_update(x,P,zf,RE,idf, 1); 
+            [x_int, P_int] = EKF_update_Intermittent(x_int, P_int, zf_int, RE, idf_int, 1, ~packetLost);
+            [x, P] = EKF_update(x, P, zf, RE, idf, 1); 
         end
         % ---------------------------------------------------------
 
-        % 添加新的landmark到状态向量中
-        [x,P]= augment(x,P, zn,RE); 
-        [x_int,P_int]= augment(x_int,P_int, zn_int,RE);
+        % Add new landmarks to the state vector
+        [x, P] = augment(x, P, zn, RE); 
+        [x_int, P_int] = augment(x_int, P_int, zn_int, RE);
     end
     
     xtrue = states(k).xtrue;
     iwp = states(k).next_keypoint;
     
-    % 清除图像
+    % Clear the plot
     cla;
     axis equal
    
-    true_trajectory(:,k) = xtrue(1:3);
-    EKF_pre_trajectory(:,k) = x(1:3);
-    EKF_pre_trajectory_int(:,k) = x_int(1:3);
+    true_trajectory(:, k) = xtrue(1:3);
+    EKF_pre_trajectory(:, k) = x(1:3);
+    EKF_pre_trajectory_int(:, k) = x_int(1:3);
     
-    % 画出历史轨迹
-    plot( true_trajectory(1, 1:k), true_trajectory(2, 1:k), 'k--','linewidth',3);
+    % Plot the historical trajectory
+    plot(true_trajectory(1, 1:k), true_trajectory(2, 1:k), 'k--', 'linewidth', 3);
     
-    % 画出历史EKF预测轨迹
-    plot( EKF_pre_trajectory(1, 1:k), EKF_pre_trajectory(2, 1:k), 'r','linewidth',2 );
+    % Plot the historical EKF predicted trajectory
+    plot(EKF_pre_trajectory(1, 1:k), EKF_pre_trajectory(2, 1:k), 'r', 'linewidth', 2);
 
-    % 画出历史中断EKF预测轨迹
-    plot( EKF_pre_trajectory_int(1, 1:k), EKF_pre_trajectory_int(2, 1:k), 'Color', [0.68, 0.85, 0.9],'linewidth',2 );
+    % Plot the historical intermittent EKF predicted trajectory
+    plot(EKF_pre_trajectory_int(1, 1:k), EKF_pre_trajectory_int(2, 1:k), 'Color', [0.68, 0.85, 0.9], 'linewidth', 2);
     
-    % 画出landmarks
-    scatter( landmarks(1, :), landmarks(2, :), 'b*' );
+    % Plot the landmarks
+    scatter(landmarks(1, :), landmarks(2, :), 'b*');
     
-    % 画出路径关键点
-    plot( wp(1,:), wp(2, :), 'r.','markersize',26 );
+    % Plot the waypoints
+    plot(wp(1, :), wp(2, :), 'r.', 'markersize', 26);
     
-    % 画出目标点的位置
-    if iwp~=0
-       plot(wp(1,iwp),wp(2,iwp),'bo','markersize',13,'linewidth',1);
+    % Plot the position of the target point
+    if iwp ~= 0
+       plot(wp(1, iwp), wp(2, iwp), 'bo', 'markersize', 13, 'linewidth', 1);
     end
     
-    % 画出车的位姿
-    draw_car(xtrue,5,'k');
+    % Plot the vehicle pose
+    draw_car(xtrue, 5, 'k');
     
-    % EKF预测位姿
-    draw_car(x,5,'r');
+    % EKF predicted pose
+    draw_car(x, 5, 'r');
 
-    % EKF预测位姿
-    draw_car(x_int,5,'b');
+    % Intermittent EKF predicted pose
+    draw_car(x_int, 5, 'b');
     
-    legend('true','ekf','int-ekf');
+    legend('true', 'ekf', 'int-ekf');
 
     pause(0.00000001)
     
     if SLAM_SAVE_GIF == 1
-        %获取当前画面
+        % Get the current frame
         F = getframe(fig);
-        %加入avi对象中
-        writeVideo(aviobj,F);
+        % Add to AVI object
+        writeVideo(aviobj, F);
         
-        %转成gif图片,只能用256色
+        % Convert to GIF image, only 256 colors can be used
         im = frame2im(F);
-        [I,map] = rgb2ind(im,256);
-        %写入 GIF89a 格式文件   
+        [I, map] = rgb2ind(im, 256);
+        % Write to GIF89a format file   
         if k == 1
-            imwrite(I,map,'ekf_slam.gif','GIF', 'Loopcount',inf,'DelayTime',0.1);
+            imwrite(I, map, 'ekf_slam.gif', 'GIF', 'Loopcount', inf, 'DelayTime', 0.1);
         else
-            imwrite(I,map,'ekf_slam.gif','GIF','WriteMode','append','DelayTime',0.1);
+            imwrite(I, map, 'ekf_slam.gif', 'GIF', 'WriteMode', 'append', 'DelayTime', 0.1);
         end
     end 
     
@@ -195,13 +194,13 @@ end
 
 %% Save results and data
 sim_result.landmarks = landmarks;
-sim_result.ture_trajectory = true_trajectory;
+sim_result.true_trajectory = true_trajectory;
 sim_result.EKF_pre_trajectory = EKF_pre_trajectory;
 sim_result.wp = wp;
 save sim_result sim_result;
 
 sim_result_int.landmarks = landmarks;
-sim_result_int.ture_trajectory = true_trajectory;
+sim_result_int.true_trajectory = true_trajectory;
 sim_result_int.EKF_pre_trajectory = EKF_pre_trajectory_int;
 sim_result_int.wp = wp;
 save sim_result_int sim_result_int;
